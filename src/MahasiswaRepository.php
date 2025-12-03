e28a70969a2b676bb30b0c8c2774283be7a91312
Rusdianaputra17 <rusdianaajoes17@gmail.com>
2025-12-03 13:36:43 +0800

    memperbaiki file MahasiswaRepository.php

--- src/MahasiswaRepository.php
+++ src/MahasiswaRepository.php
@@ -1,11 +1,17 @@
 <?php
+require_once __DIR__ . '/Mahasiswa.php';
+
 class MahasiswaRepository {
     private $conn;
-    private $table = "mahasiswa";
 
     public function __construct($db) {
         $this->conn = $db;
     }
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> c704169 (memperbaiki file MahasiswaRepository.php)
 
     public function getAll() {
         $stmt = $this->conn->prepare("SELECT * FROM $this->table ORDER BY id DESC");
@@ -34,4 +40,39 @@ class MahasiswaRepository {
         $stmt->bindParam(":id", $id);
         return $stmt->execute();
     }
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 49eea9f (menambahkan fungsi create)
+=======
+
+    // UPDATE
+    public function update($id, $data) {
+        $stmt = $this->conn->prepare("
+            UPDATE $this->table SET
+                nama = :nama,
+                nim = :nim,
+                prodi = :prodi,
+                angkatan = :angkatan,
+                status = :status
+            WHERE id = :id
+        ");
+
+        $data["id"] = $id;
+        return $stmt->execute($data);
+    }
+<<<<<<< HEAD
+>>>>>>> 36a0893 (menambahkan fungsi update)
+=======
+
+    // DELETE
+    public function delete($id) {
+        $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+        return $stmt->execute();
+    }
+>>>>>>> 8a93a0e (menambahkan fungsi delete)
+=======
+>>>>>>> 5443d5f (memperbaiki file MahasiswaRepository.php)
+>>>>>>> c704169 (memperbaiki file MahasiswaRepository.php)
 }
