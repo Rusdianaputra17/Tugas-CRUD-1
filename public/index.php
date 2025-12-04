723c23ed1be9e4e5b4b52aa3f252af7d588bf9ab
Rusdianaputra17 <rusdianaajoes17@gmail.com>
2025-12-04 15:38:24 +0800

    melanjutkan membuat index.php

--- public/index.php
+++ public/index.php
@@ -6,5 +6,35 @@ require "../src/MahasiswaController.php";
 $db = new Database();
 $repo = new MahasiswaRepository($db->connect());
 $controller = new MahasiswaController($repo);
-
 $data = $controller->index();
+?>
+    <h2>Daftar Mahasiswa</h2>
+
+    <a href="create.php" class="btn">+ Tambah Mahasiswa</a>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Prodi</th>
+            <th>Angkatan</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+
+        <?php foreach ($data as $m): ?>
+        <tr>
+            <td><?= $m['id'] ?></td>
+            <td><?= $m['nama'] ?></td>
+            <td><?= $m['nim'] ?></td>
+            <td><?= $m['prodi'] ?></td>
+            <td><?= $m['angkatan'] ?></td>
+            <td><?= $m['status'] ?></td>
+            <td class="action">
+                <a href="edit.php?id=<?= $m['id'] ?>">Edit</a>
+                <a href="delete.php?id=<?= $m['id'] ?>">Hapus</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
